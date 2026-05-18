e8e177e4c32a108c0fcc2b21afbe1aedc20a0dc9
Trai Lynne <trailynnecloud@gmail.com>
2026-05-18 14:57:54 -0600

    Upgraded `MonitorServ` to emit metric

--- runtime/servs/monitor.js
+++ runtime/servs/monitor.js
@@ -1,9 +1,11 @@
+const emitMetric = require("../../shared/utils/emitMetric");
+
 const MonitorServ = {
   serviceName: "MonitoringService",
   interval: 18, // seconds
 
   behavior() {
-    console.log(`[${this.serviceName}] CPU Spike`);
+    emitMetric(this.serviceName, "cpu_utilization", Math.floor(Math.random() * 100));
   },
 
   start() {
