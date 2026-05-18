d69956f4a1a5746e2443193cb47dfb2a38dfd824
Trai Lynne <trailynnecloud@gmail.com>
2026-05-18 12:40:25 -0600

    Created repeatable `AuthServ` service - 9 sec intervals (for testing)

--- runtime/index.js
+++ runtime/index.js
@@ -1,5 +1,10 @@
+const AuthServ = require('./servs/auth')
+
+
 console.log("[RunTime] Runtime Here")
-require('./servs/auth')
+// require('./servs/auth')
 require('./servs/billing')
 require('./servs/monitor')
 require('./servs/notfication')
+
+AuthServ.start()
--- runtime/servs/auth.js
+++ runtime/servs/auth.js
@@ -1,1 +1,20 @@
-console.log("hello from auth")
+const AuthServ = {
+  serviceName: "AuthService",
+  interval: 9, // seconds
+
+  behavior() {
+    console.log(`[${this.serviceName}] Login attempt`);
+  },
+
+  start() {
+    this.timer = setInterval(() => {
+      this.behavior();
+    }, this.interval * 1000);
+  },
+
+  stop() {
+    clearInterval(this.timer);
+  }
+};
+
+module.exports = AuthServ;
