832c5fb3bbf29e44ff6d8cc19927c1828b59d708
Trai Lynne <trailynnecloud@gmail.com>
2026-05-18 12:44:38 -0600

    Created repeatable `BillServ` service emission - 1 sec intervals (for testing)

--- runtime/index.js
+++ runtime/index.js
@@ -1,10 +1,12 @@
 const AuthServ = require('./servs/auth')
+const BillServ = require('./servs/bill')
 
 
 console.log("[RunTime] Runtime Here")
 // require('./servs/auth')
-require('./servs/billing')
+require('./servs/bill')
 require('./servs/monitor')
 require('./servs/notfication')
 
 AuthServ.start()
+BillServ.start()
