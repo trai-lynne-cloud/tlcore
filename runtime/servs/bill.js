38eec1159c147fb21367b05095d5f38c6d408768
Trai Lynne <trailynnecloud@gmail.com>
2026-05-20 11:25:30 -0600

    Upgrade `BillServ` to be affected by `queueBacklogSpike`

--- runtime/servs/bill.js
+++ runtime/servs/bill.js
@@ -1,11 +1,15 @@
 const emitMetric = require("../../shared/utils/emitMetric");
+const failInjectionState = require("../failure/state");
 
 const BillServ = {
   serviceName: "BillingService",
   interval: 10, // seconds
 
   behavior() {
-    emitMetric(this.serviceName, "billing_queue_depth", Math.floor(Math.random() * 1000));
+    let queue_depth = failInjectionState.queueBacklogSpike ?
+      Math.floor(Math.random() * 5000 - 500 + 1) + 500
+      : Math.floor(Math.random() * 151);
+    emitMetric(this.serviceName, "billing_queue_depth", queue_depth);
   },
 
   start() {
