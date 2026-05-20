baf1f97eea6f2ad2ec535b95c2b28c4d205a69af
Trai Lynne <trailynnecloud@gmail.com>
2026-05-20 11:45:59 -0600

    Upgrade `NotifyServ` to be affected by `queueBacklogSpike`

--- runtime/servs/notify.js
+++ runtime/servs/notify.js
@@ -1,11 +1,15 @@
 const emitMetric = require("../../shared/utils/emitMetric");
+const failInjectionState = require("../failure/state");
 
 const NotifyServ = {
   serviceName: "NotificationService",
   interval: 2, // seconds
 
   behavior() {
-    emitMetric(this.serviceName, "notification_queue_depth", Math.floor(Math.random() * 50));
+    let queue_depth = failInjectionState.queueBacklogSpike ?
+      Math.floor(Math.random() * 5000 - 500 + 1) + 500
+      : Math.floor(Math.random() * 151);
+    emitMetric(this.serviceName, "notification_queue_depth", queue_depth);
   },
 
   start() {
