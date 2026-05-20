ade518cd93cf9eb92924bec5569ee5bd1cdeb609
Trai Lynne <trailynnecloud@gmail.com>
2026-05-20 13:44:18 -0600

    Upgrade `NotifyServ` to be affected by `failSpike`

--- runtime/failure/state.js
+++ runtime/failure/state.js
@@ -2,6 +2,7 @@ const failInjectionState = {
   latencySpike: false,
   queueBacklogSpike: false,
   cpuSpike: false,
+  failSpike: true
 };
 
 module.exports = failInjectionState
--- runtime/servs/notify.js
+++ runtime/servs/notify.js
@@ -12,6 +12,16 @@ const NotifyServ = {
       : createRandom(0, 150);
 
     emitMetric(this.serviceName, "notification_queue_depth", queue_depth);
+
+    let failRate = failInjectionState.failSpike ?
+      createRandom(20, 100) :
+      createRandom(0, 10);
+
+    emitMetric(this.serviceName, "notification_fail_rate", failRate);
+
+    if (failInjectionState.failSpike && failRate > 80) {
+      console.log("NotificationService ERROR: simulated failure");
+    }
   },
 
   start() {
