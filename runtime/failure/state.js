040cf86dc2b346f829fbfd66644f5c0d5a978153
Trai Lynne <trailynnecloud@gmail.com>
2026-05-20 11:15:01 -0600

    Upgrade `AuthServ` to be affected by `latencySpike`

--- runtime/failure/state.js
+++ runtime/failure/state.js
@@ -3,3 +3,5 @@ const failInjectionState = {
   queueBacklogSpike: false,
   cpuSpike: false,
 };
+
+module.exports = failInjectionState
--- runtime/servs/auth.js
+++ runtime/servs/auth.js
@@ -1,11 +1,15 @@
 const emitMetric = require("../../shared/utils/emitMetric");
+const failInjectionState = require("../failure/state");
 
 const AuthServ = {
   serviceName: "AuthService",
   interval: 9, // seconds
 
   behavior() {
-    emitMetric(this.serviceName, "auth_latency", Math.floor(Math.random() * 100));
+    let latency = failInjectionState.latencySpike ?
+      Math.floor(Math.random() * (800 - 200 + 1)) + 200
+      : Math.floor(Math.random() * (60 - 10 + 1)) + 10
+    emitMetric(this.serviceName, "auth_latency", latency);
   },
 
   start() {
