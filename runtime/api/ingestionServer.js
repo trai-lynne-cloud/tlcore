39171ade3b444746aa2d23c701979d7acb34983e
Trai Lynne <trailynnecloud@gmail.com>
2026-05-21 12:21:38 -0600

    Define metric ingestion endpoint logic - calls emitMetric using req.body values

--- runtime/api/ingestionServer.js
+++ runtime/api/ingestionServer.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const emitMetric = require('../../shared/metrics/emitMetric');
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+// Ingestion Endpoint
+
 app.post('/metrics', (req, res) => {
     console.log(req.body);
+
+    emitMetric(req.body.service_id, req.body.metric_type, req.body.metric_value);
+
     res.send('Metrics received!');
 });
 
+// Port Listener 
 app.listen(port, () => {
     console.log(`Hello World from Port ${port}`);
 })
