7f243743d87221fcfb5742d4b8002112551c5099
Trai Lynne <trailynnecloud@gmail.com>
2026-05-21 12:11:45 -0600

    Create POST endpoint for metric ingestion

--- runtime/api/ingestionServer.js
+++ runtime/api/ingestionServer.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.post('/metrics', (req, res) => {
+    console.log(req.body);
+    res.send('Metrics received!');
+});
+
 app.listen(port, () => {
     console.log(`Hello World from Port ${port}`);
 })
