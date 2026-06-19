56419883760336f13d7fe90e8fd9b9330786cf8a
Trai Lynne <trailynnecloud@gmail.com>
2026-06-19 10:58:15 -0600

    Create Stable State Evaluator

---
+++ health/eval/evaluateStableState.js
@@ -0,0 +1,22 @@
+function evaluateStableState(stateHistory) {
+    if (!stateHistory || stateHistory.length === 0) {
+        return systemState.UNKNOWN;
+    }
+
+    const stateHistoryKey = {};
+
+    stateHistory.forEach(state => {
+        stateHistoryKey[state] = (stateHistoryKey[state] || 0) + 1;
+    });
+
+    if (stateHistoryKey.FAILING >= 2) {
+        return systemState.FAILING;
+    }
+
+    if (stateHistoryKey.DEGRADED >= 3) {
+        return systemState.DEGRADED;
+    }
+
+    return systemState.HEALTHY;
+
+}
