cbaf6175bc7e430146b51a12769b18e56bb3094b
Trai Lynne <trailynnecloud@gmail.com>
2026-06-24 11:32:48 -0600

    reate Update Endpoint with error handling

--- ctrl/api/failureRoutes.js
+++ ctrl/api/failureRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getFailState } = require('../failure/failureController');
+const { getFailState, enableFailure } = require('../failure/failureController');
 const router = express.Router();
 
 // Handle GET request to /control/fail-state endpoint
@@ -7,6 +7,24 @@ router.get('/control/fail-state', (req, res) => {
     const failState = getFailState();
 
     res.status(200).json(failState)
+});
+
+// Handle POST request to /control/trigger/:flag 
+router.post('/control/trigger/:flag', (req, res) => {
+    try {
+        const flag = req.params.flag
+
+        let failState = enableFailure(flag)
+
+        res.status(200).json({
+            "message": "Failure state updated",
+            "currentFailState": failState
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
 })
 
 module.exports = router;
