d57c6d4953bfa8714280feb480c3fa8976a21255
Trai Lynne <trailynnecloud@gmail.com>
2026-06-25 10:37:52 -0600

    Extend Runtime Controller

--- ctrl/runtime/runtimeController.js
+++ ctrl/runtime/runtimeController.js
@@ -16,12 +16,14 @@ function startRuntime() {
     setRuntimeStatus(runtimeStatus.RUNNING);
 
     console.log("[TLCore] Runtime started.");
+    
+    return getRuntimeStatus()
 }  
 
 function stopRuntime() {
     const status = getRuntimeStatus();
 
-    // if servise is already stopped, log a message and return
+    // if service is already stopped, log a message and return
     if (status === runtimeStatus.STOPPED) {
         console.log("[TLCore] Runtime is already stopped.");
         return;
@@ -32,10 +34,29 @@ function stopRuntime() {
     setRuntimeStatus(runtimeStatus.STOPPED);
     
     console.log("[TLCore] Runtime stopped.");
+
+    return getRuntimeStatus()
+}
+
+function restartRuntime() {
+    console.log("[TLCore] Restarting Runtime...");
+
+    stopRuntime();
+
+    if(getRuntimeStatus() !== runtimeStatus.STOPPED) throw new Error("[TLCore] Problem Stopping runtime")
+
+    startRuntime();
+
+    if (getRuntimeStatus() !== runtimeStatus.RUNNING) throw new Error("[TLCore] Problem Starting runtime")
+    
+    console.log("[TLCore] Runtime restarted successfully");
+    
+    return getRuntimeStatus();
 }
 
 module.exports = {
     getRuntimeStatus,
     startRuntime,
-    stopRuntime
+    stopRuntime,
+    restartRuntime
 };
