22133dec8dd8c139fbe48c0e09c34529ee931966
Trai Lynne Compton <trailynnecloud@gmail.com>
2026-07-27 10:55:15 -0600

    Add lifecycle fields to the incident schema - includes incident schema test

--- shared/schemas/incident.js
+++ shared/schemas/incident.js
@@ -1,11 +1,16 @@
+const incidentStatus = require("../../incidents/incidentStatus");
+const { randomUUID } = require("crypto");
+
 function createIncident(serviceId, severity) {
   return {
+    incident_id: randomUUID(),
     service_id: serviceId,
     severity,
-    start_time: new Date().toISOString()
+    start_time: new Date().toISOString(),
+    status: incidentStatus.OPEN,
   };
 }
 
 module.exports = {
-  createIncident
-}
+  createIncident,
+};
