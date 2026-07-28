5d079c8b6cb6f54bdd7fb2b29a82626b3683309d
Trai Lynne Compton <trailynnecloud@gmail.com>
2026-07-28 11:40:47 -0600

    Add incident lookup and status mutation

--- incidents/store/incidentStore.js
+++ incidents/store/incidentStore.js
@@ -1,14 +1,54 @@
 const incidentStore = [];
 
+// Create
+
 function storeIncident(incident) {
-  incidentStore.push(incident)
-};
+  incidentStore.push(incident);
+}
+
+// Read
 
 function getIncidents() {
   return [...incidentStore];
 }
 
+function getIncidentById(incidentId) {
+  const incident = incidentStore.find(
+    (incident) => incident.incident_id === incidentId,
+  );
+
+  return incident ? { ...incident } : undefined;
+}
+
+// Update
+
+function updateIncidentStatus(incidentId, nextStatus) {
+  const incidentIndex = incidentStore.findIndex(
+    (incident) => incident.incident_id === incidentId,
+  );
+
+  if (incidentIndex !== -1) {
+    incidentStore[incidentIndex] = {
+      ...incidentStore[incidentIndex],
+      status: nextStatus,
+    };
+    return { ...incidentStore[incidentIndex] };
+  }
+  return null;
+}
+
+// Test Utility
+
+function clearIncidentStore() {
+  incidentStore.length = 0;
+}
+
+// Export
+
 module.exports = {
   storeIncident,
-  getIncidents
-}
+  getIncidents,
+  getIncidentById,
+  updateIncidentStatus,
+  clearIncidentStore,
+};
